d2946ab9af441df6bd497d2282955c1df6650150
jhs512 <jangka512@gmail.com>
2021-07-27 13:05:54 +0900

    예제 1 시작

--- App.tsx
+++ App.tsx
@@ -1,15 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, View } from "react-native";
 import tw from "tailwind-react-native-classnames";
 
 export default function App() {
   return (
     <View style={tw`flex-1 bg-white justify-center items-center`}>
-      <Text style={tw`text-white font-bold`}>안녕하세요.</Text>
-      <Text style={tw`text-white font-bold`}>반갑습니다.</Text>
-      <Text style={tw`text-white`}>또 만나요.</Text>
       <StatusBar style="auto" />
+      <Button
+        onPress={() => {
+          console.log("버튼 클릭됨!");
+        }}
+        title="버튼1"
+      />
+      <View style={tw`w-20 h-20 m-10 rounded bg-red-500`}></View>
+      <View style={tw`w-20 h-20 m-10 rounded bg-green-500`}></View>
+      <View style={tw`w-20 h-20 m-10 rounded bg-blue-500`}></View>
     </View>
   );
 }
