dd78cfb03312671078a51d022302cca428f89307
jhs512 <jangka512@gmail.com>
2021-07-27 12:55:50 +0900

    테일윈드 적용

--- App.tsx
+++ App.tsx
@@ -1,24 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import tw from "tailwind-react-native-classnames";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.tsx to start working on your app!
-      </Text>
+    <View style={tw`flex-1 bg-white justify-center items-center`}>
+      <Text style={tw`text-white font-bold`}>안녕하세요.</Text>
+      <Text style={tw`text-white font-bold`}>반갑습니다.</Text>
+      <Text style={tw`text-white`}>또 만나요.</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: { backgroundColor: "red" },
-});
