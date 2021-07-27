851d57a1f8b1b8b18b9d586314d7292714cbcb6c
jhs512 <jangka512@gmail.com>
2021-07-27 13:26:07 +0900

    useState 테스트

--- App.tsx
+++ App.tsx
@@ -1,21 +1,51 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { useState } from "react";
 import { Button, View } from "react-native";
 import tw from "tailwind-react-native-classnames";
 
 export default function App() {
+  // let horMode = false;
+  const [horMode, setHorMode] = useState(false);
+
   return (
     <View style={tw`flex-1 bg-white justify-center items-center`}>
       <StatusBar style="auto" />
       <Button
         onPress={() => {
-          console.log("버튼 클릭됨!");
+          setHorMode(true);
         }}
-        title="버튼1"
+        title="반전"
       />
-      <View style={tw`w-20 h-20 m-10 rounded bg-red-500`}></View>
-      <View style={tw`w-20 h-20 m-10 rounded bg-green-500`}></View>
-      <View style={tw`w-20 h-20 m-10 rounded bg-blue-500`}></View>
+      <View style={{ height: 10 }} />
+      <Button
+        onPress={() => {
+          setHorMode(false);
+        }}
+        title="회복"
+      />
+      <View style={{ height: 10 }} />
+      <Button
+        onPress={() => {
+          setHorMode(!horMode);
+        }}
+        title="토글"
+      />
+      <View
+        style={tw.style(`w-20 h-20 m-10 rounded bg-red-500`, {
+          "bg-blue-500": horMode,
+        })}
+      />
+      <View
+        style={tw.style(`w-20 h-20 m-10 rounded bg-green-500`, {
+          "bg-yellow-500": horMode,
+        })}
+      />
+      <View
+        style={tw.style(`w-20 h-20 m-10 rounded bg-blue-500`, {
+          "bg-red-500": horMode,
+        })}
+      />
     </View>
   );
 }
